4bb3c17b997d93ba7dd0b62a124a8498eea985b7
RaduAilenei <railenei@gmail.com>
2022-04-18 10:29:39 +0300

    GRX-0000: adding download

--- dist/index.js
+++ dist/index.js
@@ -8470,7 +8470,8 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-
+const unified_agent_url = 'https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
+const unified_agent_name = 'wss-unified-agent.jar'
 const main = async () => {
     try {
 
@@ -8540,13 +8541,15 @@ const main = async () => {
             }
         }
 
-
+        console.log("Starting Download");
+        await download();
+        console.log("Finished Download");
         await octokit.rest.issues.createComment({
             owner,
             repo,
             issue_number: pr_number,
             body: `
-        Pull Request #${pr_number} has been updated with: \n
+        The Pull Request #${pr_number} has been updated with: \n
         - ${diffData.changes} changes \n
         - ${diffData.additions} additions \n
         - ${diffData.deletions} deletions \n
@@ -8555,6 +8558,74 @@ const main = async () => {
 
     } catch (error) {
         core.setFailed(error.message);
+    }
+}
+
+function getFilenameFromUrl(url) {
+    const u = new URL(url);
+    const pathname = u.pathname;
+    const pathClips = pathname.split("/");
+    const filenameWithArgs = pathClips[pathClips.length - 1];
+    return filenameWithArgs.replace(/\?.*/, "");
+}
+
+async function download() {
+    {
+        try {
+            const text = unified_agent_url;
+            const target = "/";
+            const filename = unified_agent_name;
+            let autoMatch = false;
+            if (["false", "0"].includes(autoMatch.toLowerCase().trim())) {
+                autoMatch = false;
+            } else {
+                autoMatch = true;
+            }
+            const url = (() => {
+                if (!autoMatch) return text;
+                if (autoMatch) {
+                    const match = text.match(/\((.*)\)/);
+                    if (match === null) return "";
+                    return match[1] || "";
+                }
+            })();
+            if (url.trim() === "") {
+                core.setFailed("Failed to find a URL.");
+                return;
+            }
+            console.log(`URL found: ${url}`);
+            try {
+                fs.mkdirSync(target, {
+                    recursive: true,
+                });
+            } catch (e) {
+                core.setFailed(`Failed to create target directory ${target}: ${e}`);
+                return;
+            }
+            const body = await fetch(url)
+                .then((x) => x.buffer())
+                .catch((err) => {
+                    core.setFailed(`Fail to download file ${url}: ${err}`);
+                    return undefined;
+                });
+            if (body === undefined) return;
+            console.log("Download completed.");
+            let finalFilename = "";
+            if (filename) {
+                finalFilename = String(filename);
+            } else {
+                finalFilename = getFilenameFromUrl(url);
+            }
+            if (finalFilename === "") {
+                core.setFailed("Filename not found. Please indicate it in the URL or set `filename` in the workflow.");
+                return;
+            }
+            fs.writeFileSync(path.join(target, finalFilename), body);
+            console.log("File saved.");
+            core.setOutput("filename", finalFilename);
+        } catch (error) {
+            core.setFailed(error.message);
+        }
     }
 }
 
